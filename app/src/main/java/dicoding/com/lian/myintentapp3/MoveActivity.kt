6f669f349a182123adda028ca785a03514e4ec82
revalian <revalian2@gmail.com>
2020-10-15 12:03:58 +0700

    commit

---
+++ app/src/main/java/dicoding/com/lian/myintentapp3/MoveActivity.kt
@@ -0,0 +1,11 @@
+package dicoding.com.lian.myintentapp3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
